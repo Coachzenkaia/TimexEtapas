a4c0b19f10391d75c47345265ef3cb3e745d6173
Edgar Castaneda <lisandro@Mac-mini-de-Edgar.local>
2025-06-21 21:41:25 -0600

    aa

--- js/connector.js
+++ js/connector.js
@@ -1,7 +1,50 @@
 TrelloPowerUp.initialize({
+    'card-badges': function(t, opts) {
+        return t.card('dateLastActivity')
+            .then(function(card) {
+                var lastActivity = new Date(card.dateLastActivity);
+                var now = new Date();
+                var diffMs = now - lastActivity;
+                var totalMinutes = Math.floor(diffMs / (1000 * 60));
+                
+                if (totalMinutes >= 1) {
+                    var text;
+                    var color = 'green';
+                    
+                    if (totalMinutes < 60) {
+                        text = totalMinutes + 'm';
+                    } else {
+                        var hours = Math.floor(totalMinutes / 60);
+                        var minutes = totalMinutes % 60;
+                        text = hours + 'h ' + minutes + 'm';
+                        
+                        if (hours > 24) color = 'yellow';
+                        if (hours > 72) color = 'orange';
+                        if (hours > 168) color = 'red';
+                    }
+                    
+                    return [{
+                        text: text,
+                        color: color
+                    }];
+                }
+                
+                return [];
+            })
+            .catch(function() {
+                return [];
+            });
+    },
+    
     'card-buttons': function(t, opts) {
         return [{
-            text: 'TEST BUTTON'
+            text: 'TimexEtapas',
+            callback: function(t) {
+                return t.popup({
+                    title: 'Historial TimexEtapas',
+                    url: './popup.html'
+                });
+            }
         }];
     }
 });
